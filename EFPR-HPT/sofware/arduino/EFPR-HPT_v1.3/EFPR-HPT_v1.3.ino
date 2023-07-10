2883bfd78958fa0ffa42dab68c63dc615d03ff9a
alfmh <alfonso9492@hotmail.com>
2023-07-10 20:25:27 +0200

    cosmetics

--- EFPR-HPT/sofware/arduino/EFPR-HPT_v1.3/EFPR-HPT_v1.3.ino
+++ EFPR-HPT/sofware/arduino/EFPR-HPT_v1.3/EFPR-HPT_v1.3.ino
@@ -107,72 +107,9 @@ void setup() {
   //if pressure is more than 0.5 [bar] below or above setpoint, OUTPUT will be set to min or max respectively
   myPID.setOutputRange(outputMin, outputMax);
   myPID.setBangBang (NO_BANG_BANG, NO_BANG_BANG);
-  //set PID update interval to 0.1ms
+  //set PID update init interval to 0.1ms
   myPID.setTimeStep(0.001);
   pwm(FP_PWM_OUT,PWM_FREQUENCY,(uint16_t)(output));
-/*
-  // PWM 12KHZ
-  // Set GCLK 4 to 48 MHz // REG_GCLK_GENDIV
-  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(2) |  // Divide the 48MHz clock source by divisor 2: 48MHz/2=24MHz
-                    GCLK_GENDIV_ID(4);    // Select Generic Clock (GCLK) 4
-  while (GCLK->STATUS.bit.SYNCBUSY)
-    ;  // Wait for synchronization
-
-  // Configure GCLK // REG_GCLK_GENCTLL
-  REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |          // Set the duty cycle to 50/50 HIGH/LOW
-                     GCLK_GENCTRL_GENEN |        // Enable GCLK4
-                     GCLK_GENCTRL_SRC_DFLL48M |  // Set the 48MHz clock source
-                     GCLK_GENCTRL_ID(4);         // Select GCLK4
-  while (GCLK->STATUS.bit.SYNCBUSY)
-    ;  // Wait for synchronization
-
-  // Feed GCLK4 to TCC0 and TCC1 // REG_GCLK_CLKCTRL
-  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |        // Enable GCLK4 to TCC0 and TCC1
-                     GCLK_CLKCTRL_GEN_GCLK4 |    // Select GCLK4
-                     GCLK_CLKCTRL_ID_TCC0_TCC1;  // Feed GCLK4 to TCC0 and TCC1
-  while (GCLK->STATUS.bit.SYNCBUSY)
-    ;
-  
-  //Use "Normal PWM" 
-  TCC0->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;
-  // Wait for bus synchronization
-  while (TCC0->SYNCBUSY.bit.WAVE) {};
-  // Use "Normal PWM" 
-  TCC1->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;
-  // Wait for bus synchronization
-  while (TCC1->SYNCBUSY.bit.WAVE) {};
-  // Configure PA04 (A1) to be output. 
-  PORT->Group[PORTA].DIRSET.reg = PORT_PA04;
-  PORT->Group[PORTA].OUTCLR.reg = PORT_PA04;
-
-  // Enable the peripheral multiplexer for the pins.
-  PORT->Group[0].PINCFG[4].reg |= PORT_PINCFG_PMUXEN;
-
-  PORT->Group[PORTA].PMUX[2].reg = PORT_PMUX_PMUXE_E;
-
-  // Dual slope PWM operation // REG_TCC0_WAVE
-  REG_TCC0_WAVE |= TCC_WAVE_POL(0xF) |       // Reverse the output polarity on all TCC0 outputs
-                   TCC_WAVE_WAVEGEN_DSBOTH;  // Setup dual slope PWM on TCC0
-  while (TCC0->SYNCBUSY.bit.WAVE)
-    ;  // Wait for synchronization
-
-  // Each timer counts up to a maximum or TOP value set by the PER register,
-  //Set per frecuency // REG_TCC0_PER // this determines the frequency of the PWM operation:
-  REG_TCC0_PER = 1000;  // Set the frequency of the PWM on TCC0 to 12kHz
-  while (TCC0->SYNCBUSY.bit.PER)
-    ;  // Wait for synchronization
-
-  // Set the PWM signal 20% dc // REG_TCC0_CC2
-  REG_TCC0_CC0 = 400;  // TCC0 CC3 - on A2
-  while (TCC0->SYNCBUSY.bit.CC0)
-    ;  // Wait for synchronization
-
-  // Divide the 48MHz signal by 1 giving 48MHz (20.83ns) TCC0 timer tick and enable the outputs // REG_TCC0_CTRLA
-  REG_TCC0_CTRLA |= TCC_CTRLA_PRESCALER_DIV1 |  // Divide GCLK4 by 1
-                    TCC_CTRLA_ENABLE;           // Enable the TCC0 output
-  while (TCC0->SYNCBUSY.bit.ENABLE)
-    ;  // Wait for synchronization
-  */
 }
 
 void loop() {
@@ -199,10 +136,6 @@ void loop() {
   else if (output > outputMax)
     output = outputMax;
 
-  /*REG_TCC0_CC0 = pwmOut;
-  while (TCC0->SYNCBUSY.bit.CC0)
-    ;  // Wait for synchronization
-  */
  pwm(FP_PWM_OUT,PWM_FREQUENCY,(uint16_t)(output));
 
   ledState = ((pressureBar >= (PRESSURE_SETPOINT - 0.1f)) && (pressureBar < (PRESSURE_SETPOINT + 0.1f))) ? LOW : HIGH;
